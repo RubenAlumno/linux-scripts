3bfb2de42faefd3742ad5b554647a55c1120901f
Rubén Castañeda Matute <ruben.berriozar@gmail.com>
2016-05-26 08:51:04 +0200

    Create fichero.sh

---
+++ fichero.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+while true
+do
+        write cajal
+        sleep 1
+        echo "Hola que  haces"
+        echo "¿Yo estoy haciendo un script y tu?"
+        echo "Yo tambien, hasta luego"
+        echo "Hasta luego"
+done
