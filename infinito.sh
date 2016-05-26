c633c26630ff18f02097d36528f8a0a8ace71cd9
Rubén Castañeda Matute <ruben.berriozar@gmail.com>
2016-05-26 08:52:25 +0200

    Create infinito.sh

---
+++ infinito.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Dime un nombre y me rio de él"
+read nombre
+while true
+do
+        echo ${nombre}
+        echo ja,ja, ja
+        sleep 1
+        echo je, je, je
+        sleep 1
+done
